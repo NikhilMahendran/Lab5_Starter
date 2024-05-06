6fc4d2d8590bb532c4732f956e7e5bff105b57b4
NikhilMahendran <150480880+NikhilMahendran@users.noreply.github.com>
2024-05-05 21:57:53 -0700

    attempt 1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,49 @@
-// explore.js
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let synth = window.speechSynthesis;
+  let voices = [];
+  let voice_select = document.querySelector("#voice-select");
+  let button = document.querySelector("button");
+  let text = document.querySelector("#text-to-speak");
+  let img = document.querySelector("img");
+
+  // from web API documentation
+    function populateVoices() {
+    for(let i = 0; i < voices.length; i++){
+      let option = document.createElement("option");
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+  
+      if(voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+  
+      voice_select.appendChild(option);
+  
+    }
+  }
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  button.addEventListener("click", function() {
+    let utterance = new SpeechSynthesisUtterance(text.value);
+    let selectedOption = voice_select.selectedOptions[0].getAttribute('data-name');
+    for(let i = 0; i < voices.length; i++){
+      if(voices[i].name === selectedOption){
+        utterance.voice = voices[i];
+      }
+    }
+    synth.speak(utterance);
+    img.src = "assets/images/smiling-open.png";
+    utterance.onend = function() {
+      img.src = "assets/images/smiling.png";
+    };
+  });
+  
 }
