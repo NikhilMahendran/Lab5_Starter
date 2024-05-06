38a189f05fc3652ea912c8627c4b06a35325f097
NikhilMahendran <150480880+NikhilMahendran@users.noreply.github.com>
2024-05-05 20:35:36 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,10 @@
-// expose.js
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var hornselect = document.querySelector("#horn-select");
+  var hornimage = document.getElementsByTagName("img").item(0);
+  var audio = document.getElementsByTagName("audio").item(0);
+  const confetti = new JSConfetti()
+  
 }
